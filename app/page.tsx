497c1299fbc599d7bf71c3544e92071f424c16b2
Enes Demirel <enesdemirelus@gmail.com>
2025-09-13 22:12:10 -0500

    downloaded zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex h-[calc(100vh-3.55rem)] overflow-hidden">
-      <div className="h-full w-1/2 bg-blue-500"></div>
-      <div className="h-full w-1/2 bg-red-500"></div>
+    <div className="flex h-[calc(100vh-4rem)] w-full overflow-hidden gap-2 p-2">
+      <div className="h-full w-1/2 border border-gray-300 rounded-lg shadow-sm bg-white"></div>
+      <div className="h-full w-1/2 border border-gray-300 rounded-lg shadow-sm bg-white"></div>
     </div>
   );
 }
